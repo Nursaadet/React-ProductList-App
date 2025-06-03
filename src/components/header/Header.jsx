2d39f1d0d662902ccfb789c7b740807631485613
Nursaadet <nursaadetozdemir@gmail.com>
2025-06-03 18:21:13 +0300

    Updated App.scss and imported it into App.js

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,16 @@
-const Header =(props)=>{
-    console.log(props)
-    return(
-        <div>
-            <h1>Header</h1>
-        </div>
-    )
-}
+import HeaderStyles from "./Header.module.scss";
+
+const Header = ({categories, text}) => {
+  return (
+    <div className={HeaderStyles.header}>
+      <h1>{text}</h1>
+      <div className={HeaderStyles.btns}>
+        {categories.map((item) => (
+          <button key={item}>{item}</button>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Header;
