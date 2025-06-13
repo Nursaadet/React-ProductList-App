f1e3d55591be89b9590f0407e706f855570bb049
Nursaadet <nursaadetozdemir@gmail.com>
2025-06-14 00:27:04 +0300

    added new feature which is SearchBox

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import Header from "./components/header/Header";
 import ProductList from "./components/products/ProductList";
-import"./App.scss";
 import { products, categories } from "./helper/data";
+import "./App.scss";
 
 function App() {
-  console.log(products, categories);
+  // console.log(products, categories);
   return (
     <div className="App">
       <Header categories={categories} text="ProductsList" />
-      <ProductList products={products} />
     </div>
   );
 }
--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -1,13 +1,23 @@
 import ProductCard from "./ProductCard";
 import "./Products.scss";
 
-const ProductList = ({ products }) => {
+const ProductList = ({ products, inputVal }) => {
+  console.log(inputVal);
   console.log(products);
+  const filteredData = products.filter((item) =>
+    item.title.toLowerCase().includes((inputVal|| "").trim().toLowerCase())
+  );
   return (
     <div className="product-list">
-      {products.map((product) => (
+      {filteredData.map((product) => (
         <ProductCard key={product.id} {...product} />
       ))}
+
+      {!filteredData.length && (
+        <div>
+          <h1> Product not found...</h1>
+        </div>
+      )}
     </div>
   );
 };
