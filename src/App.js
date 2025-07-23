fa08a56fbc5da682488cd251c1c86b9c15a83cc6
Nursaadet <nursaadetozdemir@gmail.com>
2025-07-23 16:23:03 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/header/Header";
+import ProductList from "./components/products/ProductList";
+
+import {products, categories} from "./helper/data";
+function App() {
+  // console.log(products, categories);
+  return (
+    <div className="App">
+      <Header categories={categories} text= "Product List" />
+      <ProductList products={products} />
+    </div>
+  );
+}
+
+export default App;
